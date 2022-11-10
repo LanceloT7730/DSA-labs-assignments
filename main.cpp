a3a96fbfbbb4292f1a794a05aea10c98531b832c
210046 <210046@akfauniversity.org>
2022-11-10 19:00:59 +0500

    task 1 completed

--- main.cpp
+++ main.cpp
@@ -2,126 +2,124 @@
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node *next;
-
-    Node() {
-        next = NULL;
-    }
-};
-
-// task 1
-class List {
-private:
-    Node *head;
+template<class Type>
+class queueType {
 public:
-    List() {
-        head = NULL;
+    bool isEmptyQueue() {
+        return count == 0;
     }
 
-    void insertAtFront(int data) {
-        Node *newNode = new Node;
-        newNode->data = data;
-        newNode->next = head;
-        head = newNode;
+    bool isFullQueue() {
+        return count == maxQueueSize;
     }
 
-    void printAll() {
-        Node *temp = head;
+    void initializeQueue() {
+        queueRear = -1;
+        queueFront = -1;
+        count = 0;
+        list = new Type[maxQueueSize];
+    }
 
-        while (temp) {
-            cout << temp->data << endl;
-            temp = temp->next;
+    Type front() {
+        cout << "Front: " << list[queueFront] << endl;
+        return list[queueFront];
+    }
+
+    Type back() {
+        cout << "Back: " << list[queueRear] << endl;
+        return list[queueRear];
+    }
+
+
+    void addQueue(const Type &queueElement) {
+        if (!isFullQueue()) {
+            queueRear = (queueRear + 1) % maxQueueSize;
+            if (queueFront == -1) queueFront = (queueFront + 1) % maxQueueSize;
+            count++;
+            list[queueRear] = queueElement;
         }
     }
-};
 
-// task 2
-struct DoubleNode {
-    int data;
-    DoubleNode *next, *prev;
-
-    DoubleNode() {
-        next = NULL;
-        prev = NULL;
-    }
-};
-
-class DoubleLinkedList : public List {
-private:
-    DoubleNode *head, *tail;
-public:
-    DoubleLinkedList() {
-        head = NULL;
-        tail = NULL;
-    }
-
-    void DoubleFrontInsertion(int data) {
-        DoubleNode *newDoubleNode = new DoubleNode;
-        newDoubleNode->data = data;
-        if (head) {
-            newDoubleNode->prev = head->prev;
-            newDoubleNode->next = head;
-            head = newDoubleNode;
-        } else {
-            newDoubleNode->prev = head;
-            newDoubleNode->next = head;
-
-            head = newDoubleNode;
+    void deleteQueue() {
+        if (!isEmptyQueue()) {
+            queueFront = (queueFront + 1) % maxQueueSize;
+            if (queueRear == 0) queueRear = (queueRear - 1) % maxQueueSize;
+            count--;
         }
     }
-};
 
-//task 3
-class CircularLinkedList : public List {
-private:
-    Node *head;
-public:
-    CircularLinkedList() {
-        head = NULL;
-    }
-
-    void CircularFrontInsertion(int data) {
-        Node *newNode = new Node;
-        newNode->data = data;
-        if (!head) {
-            insertAtFront(data);
-            return;
+    void printQueue() {
+        for (int i = 0; i < maxQueueSize; i++) {
+            cout << list[queueFront + i] << endl;
         }
-        newNode->next = head;
-        head = newNode;
 
-        Node *temp = head->next;
-
-        do {
-            temp = temp->next;
-            if (temp == newNode->next) temp = newNode;
-        } while (temp);
     }
+
+    queueType(int queueSize = 10) { // Function with default parameters
+        maxQueueSize = queueSize;
+    }
+
+    queueType(const queueType<Type> &otherQueue) {
+        maxQueueSize = otherQueue.maxQueueSize;
+        initializeQueue();
+        for (int i = 0; i < maxQueueSize; i++) {
+            addQueue(otherQueue.list[queueFront + i]);
+        }
+    }
+
+// To create a Queue object that copies another queue
+    ~queueType() {
+
+    }
+
+private:
+    int maxQueueSize;
+    int count;
+    int queueFront;
+    int queueRear;
+    Type *list;
 };
+
+
 
 int main() {
-    // testing task 1
-    List obj;
-    obj.insertAtFront(5);
-    obj.insertAtFront(4);
-    obj.printAll();
+    queueType<int> Q1;
+    Q1.initializeQueue();
+    cout << "Is the queue empty? " << Q1.isEmptyQueue() << endl; // output is 1 as it is empty for now
 
-    // testing task 2
-    DoubleLinkedList obj_double;
-    obj_double.DoubleFrontInsertion(5);
-    obj_double.DoubleFrontInsertion(4);
-    obj_double.printAll();
+    Q1.addQueue(15);
+    Q1.addQueue(25);
+    Q1.addQueue(4);
+    Q1.addQueue(12);
+    Q1.addQueue(123);
+    Q1.addQueue(75);
+    Q1.addQueue(85);
+    Q1.addQueue(55);
+    Q1.front(); // should display 15
+    Q1.deleteQueue();
 
-    // testing task 3
-    CircularLinkedList obj_circular;
-    obj_circular.CircularFrontInsertion(5);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.printAll();
+    Q1.printQueue();// should display all the queue element in the order they are added
+    // except 15
 
+    cout << endl << "Second part started" << endl;
+    queueType<int> Q2(Q1);
+    Q1.addQueue(1000);
+    Q1.addQueue(2000);
+    Q1.printQueue(); // Should display all the members of Q1 in the order they are
+    // inserted except 15 as it was deleted earlier
+    Q2.printQueue();
+// Should display all the members of Q2 i-e all the members of Q1 except for 1000
+// and 2000
+    Q1.initializeQueue();
+    Q1.printQueue();
+//Should display a message &quot;An empty Queue can&#39;t be displayed&quot;
+    Q1.addQueue(35);
+    cout << Q1.front() << endl;
+//Should display 35
+    cout << Q1.front() << endl;
+//Should display 35
+    Q1.addQueue(3);
+    Q1.printQueue();
+//Should display both 35 and 3
     return 0;
 }
