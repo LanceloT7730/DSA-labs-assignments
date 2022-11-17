631256af9bffb929a67fe3552ed16baf623d262a
Doniyorbek <210046@akfauniversity.org>
2022-11-17 16:23:46 +0500

    task 1 completed

--- main.cpp
+++ main.cpp
@@ -2,133 +2,107 @@
 
 using namespace std;
 
-/*struct Node {
-    int data;
-    Node *next;
+
+
+// task 1
+// what is the difference between typename and class in template
+template <class Type>
+class StackType {
+public:
+    StackType(int stackSize = 10) {
+        maxStackSize = stackSize;
+        list = new Type[maxStackSize];
+        CreateStack();
+    }
+
+    void CreateStack() {
+        stackTop = -1;
+        count = 0;
+    }
+
+    bool isStackEmpty() {
+        return count == 0;
+    }
+
+    bool isStackFull() {
+        return count == maxStackSize;
+    }
+    void push(Type data) {
+        if(!isStackFull()) {
+            stackTop++;
+            list[stackTop] = data;
+            count++;
+        } else {
+            cout << "Cannot add to full stack" << endl;
+        }
+    }
+    void pop(){
+        if(!isStackEmpty()) {
+            stackTop--;
+            count--;
+        } else {
+            cout << "Cannot remove element from empty list" << endl;
+        }
+    }
+
+    Type top() {
+        return list[stackTop];
+    }
+
+    int CountStackElements(){
+        return count;
+    }
+
+    void DestroyStack() {
+        CreateStack();
+    }
+
+    void printAll() {
+        int size = count;
+        for (int i = 0; i < size; i++) {
+            cout << top();
+            pop();
+        }
+    }
+
+private:
+    int stackTop;
+    int maxStackSize;
+    int count;
+    Type *list;
+};
+
+template <class Type>
+struct Node {
+    Type data;
+    Node <Type> *next;
+    string data_string;
 
     Node() {
         next = NULL;
     }
 };
-
-// task 1
-class List {
+template <class Type>
+class LinkedListStack {
 private:
-    Node *head;
+    Node <Type> *head;
+    int count = 0;
 public:
-    List() {
+    LinkedListStack() {
         head = NULL;
     }
 
-    void insertAtFront(int data) {
-        Node *newNode = new Node;
-        newNode->data = data;
-        newNode->next = head;
-        head = newNode;
-    }
-
-    void printAll() {
-        Node *temp = head;
-
-        while (temp) {
-            cout << temp->data << endl;
-            temp = temp->next;
-        }
-    }
 };
-
-// task 2
-struct DoubleNode {
-    int data;
-    DoubleNode *next, *prev;
-
-    DoubleNode() {
-        next = NULL;
-        prev = NULL;
-    }
-};
-
-class DoubleLinkedList : public List {
-private:
-    DoubleNode *head, *tail;
-public:
-    DoubleLinkedList() {
-        head = NULL;
-        tail = NULL;
-    }
-
-    void DoubleFrontInsertion(int data) {
-        DoubleNode *newDoubleNode = new DoubleNode;
-        newDoubleNode->data = data;
-        if (head) {
-            newDoubleNode->prev = head->prev;
-            newDoubleNode->next = head;
-            head = newDoubleNode;
-        } else {
-            newDoubleNode->prev = head;
-            newDoubleNode->next = head;
-
-            head = newDoubleNode;
-        }
-    }
-};
-
-//task 3
-class CircularLinkedList : public List {
-private:
-    Node *head;
-public:
-    CircularLinkedList() {
-        head = NULL;
-    }
-
-    void CircularFrontInsertion(int data) {
-        Node *newNode = new Node;
-        newNode->data = data;
-        if (!head) {
-            insertAtFront(data);
-            return;
-        }
-        newNode->next = head;
-        head = newNode;
-
-        Node *temp = head->next;
-
-        do {
-            temp = temp->next;
-            if (temp == newNode->next) temp = newNode;
-        } while (temp);
-    }
-};*/
-// Create a program that takes name of a user and print it in reverse order.
-template<class Type>
-class stackType{
-
-};
-
 int main() {
-/*
     // testing task 1
-    List obj;
-    obj.insertAtFront(5);
-    obj.insertAtFront(4);
-    obj.printAll();
-
-    // testing task 2
-    DoubleLinkedList obj_double;
-    obj_double.DoubleFrontInsertion(5);
-    obj_double.DoubleFrontInsertion(4);
-    obj_double.printAll();
-
-    // testing task 3
-    CircularLinkedList obj_circular;
-    obj_circular.CircularFrontInsertion(5);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.CircularFrontInsertion(6);
-    obj_circular.printAll();
-*/
-
+    /*StackType<char> stack;
+    string name;
+    cout << "Enter your name to be printed reversely" << endl;
+    cin >> name;
+    for (int i = 0; i < name.size(); i++) {
+        stack.push(name[i]);
+    }
+    cout << "Result: ";
+    stack.printAll();*/
     return 0;
 }
