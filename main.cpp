22660c4610e402dfa5182b9d831399779f021d37
Doniyorbek <210046@akfauniversity.org>
2022-11-17 17:52:49 +0500

    Task 3 completed

--- main.cpp
+++ main.cpp
@@ -122,29 +122,40 @@ public:
         if (isStackEmpty()) {
             newNode->next = NULL;
             top = newNode;
-            count++;
         } else {
             newNode->next = top;
             top = newNode;
-            count++;
         }
+        count++;
     }
 
     void pop() {
         if (!isStackEmpty()) {
             Node<Type> *temp = top;
-            while (temp) {
-                top = top->next;
-                delete temp;
-                temp = top;
-            }
-        }
-        else cout << "Cannot delete element from empty list\n";
+            //while (temp) {
+            top = top->next;
+            delete temp;
+            temp = top;
+            count--;
+            //}
+        } else cout << "Cannot delete element from empty list\n";
 
     }
 
     Type Top() {
         return top->data;
+    }
+
+    int CountStackElements() {
+        return count;
+    }
+
+    void DestroyStack() {
+        Node<Type> *temp = top;
+        while (count != 0) {
+            pop();
+            count--;
+        }
     }
 
     void printAll() {
@@ -157,6 +168,30 @@ public:
         cout << endl;
     }
 
+    bool isPalindrome(string data) {
+        bool answer = false;
+        // copying all data characters to the stack
+        for (char &i: data) push(i);
+
+        // checking if the num of characters is odd or even
+        if(data.size() % 2 == 0) {
+            for (char &i: data) {
+                if (top->data == i) {
+                    answer = true;
+                    top = top->next;
+                } else return false;
+            }
+            return answer;
+        } else {
+            for (int i = 0; i < (data.size() - 1); i++) {
+                if (top->data == data[i]) {
+                    answer = true;
+                    top = top->next;
+                } else return false;
+            }
+            return answer;
+        }
+    }
 };
 
 int main() {
@@ -187,9 +222,22 @@ int main() {
 
     cout << "Top element is: " << stack.Top() << endl;
 
-    cout << "Deleting elements from the stack\n";
+    cout << "Deleting one element from the stack\n";
     stack.pop();
     cout << "Stack elements are\n";
     stack.printAll();
+
+    cout << "Number of elements in stack: " << stack.CountStackElements() << endl;
+
+    cout << "Destroying the stack\n";
+    stack.DestroyStack();
+    cout << "Number of elements in stack: " << stack.CountStackElements() << endl;
+
+    string palindrome = "Something";
+    cout << "Checking whether a string " << palindrome <<  " is palindrome or not: " << stack.isPalindrome(palindrome) << endl;
+
+    palindrome = "ACCA";
+    cout << "Checking whether a string " << palindrome <<  " is palindrome or not: " << stack.isPalindrome(palindrome) << endl;
+
     return 0;
 }
